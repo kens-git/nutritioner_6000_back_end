1a2b93f022a6c4e031178d44d8c1ced8f7dd967a
Ken Montgomery <ID+username@users.noreply.github.com>
2022-07-21 10:49:16 -0400

    Registered models for admin panel.

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from app.models import *
 
-# Register your models here.
+admin.site.register(Consumable)
+admin.site.register(DailyValue)
+admin.site.register(Food)
+admin.site.register(FoodCategory)
+admin.site.register(Intake)
+admin.site.register(NutrientUnit)
+admin.site.register(Target)
+admin.site.register(UnitOfMeasure)
