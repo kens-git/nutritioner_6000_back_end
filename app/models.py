47bf7d50189b29de86d4639edc3841a22e251c18
Ken Montgomery <ID+username@users.noreply.github.com>
2022-08-05 18:37:50 -0400

    Improved models by removing duplication, and updated them to support custom units, categories, daily values, and nutrients.

--- app/models.py
+++ app/models.py
@@ -2,88 +2,47 @@ from django.conf import settings
 from django.db import models
 from django.forms import JSONField
 
-class UnitOfMeasure(models.Model):
+class Abbreviation(models.Model):
   name = models.TextField(unique=True) # TODO: case insensitive
-  abbreviation = models.TextField(unique=True) # TODO: case insensitive
+  abbreviation = models.TextField()
 
-  def __str__(self):
-    return self.name
+class UnitOfMeasure(models.Model):
+  name = models.TextField(unique=True, blank=False)
 
-class NutrientUnit(models.Model):
-  nutrient_name = models.TextField()
-  unit = models.ForeignKey(UnitOfMeasure, on_delete=models.CASCADE)
+class Nutrient(models.Model):
+  name = models.TextField(unique=True)
+  abbreviation = models.ForeignKey(Abbreviation, on_delete=models.PROTECT)
+  description = models.TextField()
+  unit_of_measure = models.ForeignKey(UnitOfMeasure, on_delete=models.PROTECT)
 
 class DailyValue(models.Model):
-  nutrient_name = models.TextField()
+  user = models.ForeignKey(
+    settings.AUTH_USER_MODEL,
+    on_delete=models.CASCADE, null=True, blank=True)
+  nutrient = models.OneToOneField(Nutrient, unique=True, on_delete=models.CASCADE)
   value = models.FloatField()
 
+class ConsumableCategory(models.Model):
+  name = models.TextField()
+  abbreviation = models.ForeignKey(Abbreviation, on_delete=models.PROTECT)
+  description = models.TextField()
+
 class Consumable(models.Model):
-  fat = models.FloatField()
-  saturated_fat = models.FloatField()
-  trans_fat = models.FloatField()
-  cholesterol = models.FloatField()
-  sodium = models.FloatField()
-  carbohydrates = models.FloatField()
-  fibre = models.FloatField()
-  sugar = models.FloatField()
-  protein = models.FloatField()
-  biotin = models.FloatField()
-  calcium = models.FloatField()
-  chromium = models.FloatField()
-  copper = models.FloatField()
-  folacin = models.FloatField()
-  iodide = models.FloatField()
-  iron = models.FloatField()
-  magnesium = models.FloatField()
-  manganese = models.FloatField()
-  molybdenum = models.FloatField()
-  niacin = models.FloatField()
-  pantothenate = models.FloatField()
-  phosphorous = models.FloatField()
-  potassium = models.FloatField()
-  riboflavin = models.FloatField()
-  selenium = models.FloatField()
-  thiamine = models.FloatField()
-  vitamin_a = models.FloatField()
-  vitamin_b6 = models.FloatField()
-  vitamin_b12 = models.FloatField()
-  vitamin_c = models.FloatField()
-  vitamin_d = models.FloatField()
-  vitamin_e = models.FloatField()
-  zinc = models.FloatField()
-
-  class Meta:
-    abstract = True
-
-class FoodCategory(models.Model):
-  name = models.TextField(unique=True)
-
-  def __str__(self):
-    return self.name
-
-class Food(Consumable):
-  name = models.TextField(unique=True)
-  category = models.ForeignKey(FoodCategory, on_delete=models.CASCADE)
-  unit = models.ForeignKey(UnitOfMeasure, on_delete=models.CASCADE)
+  name = models.TextField()
+  category = models.ForeignKey(ConsumableCategory, on_delete=models.PROTECT)
+  unit = models.ForeignKey(UnitOfMeasure, on_delete=models.PROTECT)
   reference_size = models.FloatField()
+  nutrients = models.ManyToManyField(Nutrient)
 
-  def __str__(self):
-    return self.name
-
-class Target(Consumable):
+class Target(models.Model):
   user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
   timestamp = models.DateTimeField()
-  name = models.TextField(unique=True)
-
-  def __str__(self):
-    return '{}: {}'.format(self.user, self.name)
+  name = models.TextField()
+  description = models.TextField()
+  nutrients = models.ManyToManyField(Nutrient)
 
 class Intake(models.Model):
   user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
   timestamp = models.DateTimeField()
-  food = models.ForeignKey(Food, on_delete=models.CASCADE)
+  consumable = models.ForeignKey(Consumable, on_delete=models.PROTECT)
   serving_size = models.FloatField()
-
-  def __str__(self):
-    return '{}: {} of {} at {}'.format(
-      self.user, self.serving_size, self.food, self.timestamp)
