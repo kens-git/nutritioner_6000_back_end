1efdbeb3500c1fa87dd3eaceeb2dc662c9e780f7
Ken Montgomery <ID+username@users.noreply.github.com>
2022-08-10 14:29:27 -0400

    Updated models to refer to a single Name field that contains a name, plural, and abbreviation.

--- app/models.py
+++ app/models.py
@@ -1,43 +1,39 @@
 from django.conf import settings
 from django.db import models
-from django.forms import JSONField
 
-class Abbreviation(models.Model):
-  name = models.TextField(unique=True) # TODO: case insensitive
-  abbreviation = models.TextField()
-
-class UnitOfMeasure(models.Model):
-  name = models.TextField(unique=True, blank=False)
+class Name(models.Model):
+  name = models.CharField(unique=True, max_length=200) # TODO: case insensitive
+  abbreviation = models.CharField(max_length=15)
+  plural = models.CharField(max_length=50)
 
 class Nutrient(models.Model):
-  name = models.TextField(unique=True)
-  abbreviation = models.ForeignKey(Abbreviation, on_delete=models.PROTECT)
+  name = models.ForeignKey(Name, on_delete=models.PROTECT)
   description = models.TextField()
-  unit_of_measure = models.ForeignKey(UnitOfMeasure, on_delete=models.PROTECT)
+  unit = models.ForeignKey(Name, on_delete=models.PROTECT)
 
 class DailyValue(models.Model):
+  # TODO: instead of nullable, maybe add admin user as creator
   user = models.ForeignKey(
     settings.AUTH_USER_MODEL,
     on_delete=models.CASCADE, null=True, blank=True)
-  nutrient = models.OneToOneField(Nutrient, unique=True, on_delete=models.CASCADE)
+  nutrient = models.ForeignKey(Nutrient, on_delete=models.CASCADE)
   value = models.FloatField()
 
 class ConsumableCategory(models.Model):
-  name = models.TextField()
-  abbreviation = models.ForeignKey(Abbreviation, on_delete=models.PROTECT)
+  name = models.ForeignKey(Name, on_delete=models.PROTECT)
   description = models.TextField()
 
 class Consumable(models.Model):
   name = models.TextField()
   category = models.ForeignKey(ConsumableCategory, on_delete=models.PROTECT)
-  unit = models.ForeignKey(UnitOfMeasure, on_delete=models.PROTECT)
+  unit = models.ForeignKey(Name, on_delete=models.PROTECT)
   reference_size = models.FloatField()
   nutrients = models.ManyToManyField(Nutrient)
 
 class Target(models.Model):
   user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
   timestamp = models.DateTimeField()
-  name = models.TextField()
+  name = models.CharField(unique=True, max_length=200)
   description = models.TextField()
   nutrients = models.ManyToManyField(Nutrient)
 
