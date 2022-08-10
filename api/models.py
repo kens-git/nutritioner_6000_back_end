9b0e1db641fd613d9bba6642486bde7ef47e4ec8
Ken Montgomery <ID+username@users.noreply.github.com>
2022-08-10 16:39:41 -0400

    Model updates:
    Added __str__ methods.
    Fixed typo in Nutrient.name's related_name.
    Added a ConsumableNutrient model to store a value along with a Nutrient.

--- api/models.py
+++ api/models.py
@@ -6,12 +6,18 @@ class Name(models.Model):
   abbreviation = models.CharField(max_length=15)
   plural = models.CharField(max_length=50)
 
+  def __str__(self):
+    return f'{self.name} ({self.abbreviation})'
+
 class Nutrient(models.Model):
   name = models.ForeignKey(
-    Name, on_delete=models.PROTECT, related_name='nutreint_name')
+    Name, on_delete=models.PROTECT, related_name='nutrient_name')
   description = models.TextField()
   unit = models.ForeignKey(
     Name, on_delete=models.PROTECT, related_name='nutrient_unit')
+
+  def __str__(self):
+    return f'{self.name} ({self.unit})'
 
 class DailyValue(models.Model):
   # TODO: instead of nullable, maybe add admin user as creator
@@ -21,26 +27,49 @@ class DailyValue(models.Model):
   nutrient = models.ForeignKey(Nutrient, on_delete=models.CASCADE)
   value = models.FloatField()
 
+  def __str__(self):
+    return f'{self.nutrient} ({self.value})'
+
 class ConsumableCategory(models.Model):
   name = models.ForeignKey(Name, on_delete=models.PROTECT)
   description = models.TextField()
+
+  def __str__(self):
+    return f'{self.name}'
+
+class ConsumableNutrient(models.Model):
+  nutrient = models.ForeignKey(Nutrient, on_delete=models.PROTECT)
+  value = models.FloatField()
+
+  def __str__(self):
+    return f'{self.nutrient} ({self.value})'
 
 class Consumable(models.Model):
   name = models.TextField()
   category = models.ForeignKey(ConsumableCategory, on_delete=models.PROTECT)
   unit = models.ForeignKey(Name, on_delete=models.PROTECT)
   reference_size = models.FloatField()
-  nutrients = models.ManyToManyField(Nutrient)
+  nutrients = models.ManyToManyField(
+    ConsumableNutrient, on_delete=models.PROTECT)
+
+  def __str__(self):
+    return f'{self.name}'
 
 class Target(models.Model):
   user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
   timestamp = models.DateTimeField()
   name = models.CharField(unique=True, max_length=200)
-  description = models.TextField()
+  description = models.TextField(blank=True)
   nutrients = models.ManyToManyField(Nutrient)
+
+  def __str__(self):
+    return f'{self.user}: {self.description}'
 
 class Intake(models.Model):
   user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
   timestamp = models.DateTimeField()
   consumable = models.ForeignKey(Consumable, on_delete=models.PROTECT)
   serving_size = models.FloatField()
+
+  def __str__(self):
+    return f'{self.user}, {self.timestamp}, {self.consumable}, {self.serving_size}'
