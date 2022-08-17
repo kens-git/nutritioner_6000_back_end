e19353d1d0a0e96af6c99b1af3c2f93852fe52bd
Ken Montgomery <ID+username@users.noreply.github.com>
2022-08-17 10:56:53 -0400

    Added UserCascade and UserProtect models for User foreign keys.

--- api/models.py
+++ api/models.py
@@ -1,7 +1,19 @@
 from django.conf import settings
 from django.db import models
 
-class Name(models.Model):
+class UserCascade(models.Model):
+  user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+  class Meta:
+    abstract = True
+
+class UserProtect(models.Model):
+  user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+
+  class Meta:
+    abstract = True
+
+class Name(UserProtect):
   name = models.CharField(unique=True, max_length=200) # TODO: case insensitive
   abbreviation = models.CharField(max_length=15)
   plural = models.CharField(max_length=50)
@@ -9,9 +21,9 @@ class Name(models.Model):
   def __str__(self):
     return f'{self.name} ({self.abbreviation})'
 
-class Nutrient(models.Model):
+class Nutrient(UserProtect):
   name = models.ForeignKey(
-    Name, on_delete=models.PROTECT, related_name='nutrient_name')
+    Name, unique=True, on_delete=models.PROTECT, related_name='nutrient_name')
   description = models.TextField(blank=True)
   unit = models.ForeignKey(
     Name, on_delete=models.PROTECT, related_name='nutrient_unit')
@@ -19,19 +31,15 @@ class Nutrient(models.Model):
   def __str__(self):
     return f'{self.name} ({self.unit})'
 
-class DailyValue(models.Model):
-  # TODO: instead of nullable, maybe add admin user as creator
-  user = models.ForeignKey(
-    settings.AUTH_USER_MODEL,
-    on_delete=models.CASCADE, null=True, blank=True)
+class DailyValue(UserProtect):
   nutrient = models.ForeignKey(Nutrient, on_delete=models.CASCADE)
   value = models.FloatField()
 
   def __str__(self):
     return f'{self.nutrient} ({self.value})'
 
-class ConsumableCategory(models.Model):
-  name = models.ForeignKey(Name, on_delete=models.PROTECT)
+class ConsumableCategory(UserProtect):
+  name = models.ForeignKey(Name, unique=True, on_delete=models.PROTECT)
   description = models.TextField(blank=True)
 
   def __str__(self):
@@ -44,18 +52,17 @@ class ConsumableNutrient(models.Model):
   def __str__(self):
     return f'{self.nutrient} ({self.value})'
 
-class Consumable(models.Model):
-  name = models.TextField()
+class Consumable(UserProtect):
+  name = models.TextField(unique=True)
   category = models.ForeignKey(ConsumableCategory, on_delete=models.PROTECT)
-  unit = models.ForeignKey(Name, on_delete=models.PROTECT)
+  unit = models.ForeignKey(Name, related_name='unit', on_delete=models.PROTECT)
   reference_size = models.FloatField()
   nutrients = models.ManyToManyField(ConsumableNutrient)
 
   def __str__(self):
     return f'{self.name}'
 
-class Target(models.Model):
-  user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+class Target(UserCascade):
   timestamp = models.DateTimeField()
   name = models.CharField(unique=True, max_length=200)
   description = models.TextField(blank=True)
@@ -64,8 +71,7 @@ class Target(models.Model):
   def __str__(self):
     return f'{self.user}: {self.description}'
 
-class Intake(models.Model):
-  user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+class Intake(UserCascade):
   timestamp = models.DateTimeField()
   consumable = models.ForeignKey(Consumable, on_delete=models.PROTECT)
   serving_size = models.FloatField()
