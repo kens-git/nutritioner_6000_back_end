db35cc5e28f43040c4b14dc35ec8af7739d2c140
Ken Montgomery <ID+username@users.noreply.github.com>
2022-08-17 22:40:49 -0400

    Registered models with the admin site.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import (Consumable, ConsumableCategory, ConsumableNutrient,
+  DailyValue, Intake, Name, Nutrient, Target)
 
-# Register your models here.
+admin.site.register(Consumable)
+admin.site.register(ConsumableCategory)
+admin.site.register(ConsumableNutrient)
+admin.site.register(DailyValue)
+admin.site.register(Intake)
+admin.site.register(Name)
+admin.site.register(Nutrient)
+admin.site.register(Target)
