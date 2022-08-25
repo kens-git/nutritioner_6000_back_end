6ae61cccfa67ec548bccbc2ea1ddad7cf78c81af
Ken Montgomery <ID+username@users.noreply.github.com>
2022-08-25 11:26:33 -0400

    Changed default DRF authentication class to TokenAuthentication.

--- nutrition_tracker/settings.py
+++ nutrition_tracker/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+# TODO: fix indenting
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -131,3 +132,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+  'DEFAULT_AUTHENTICATION_CLASSES': [
+    'rest_framework.authentication.TokenAuthentication'],
+}
