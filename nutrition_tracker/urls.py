4ca9dd721223941c57319e4f9f30375ed07680e5
Ken Montgomery <ID+username@users.noreply.github.com>
2022-10-29 21:07:12 -0400

    Added catch-all URL in the back end to return the index view for every un-handled URL. This prevents URLs like domain.com/foo from returning 404 because they don't exist on the back end, but were instead created by react-router.

--- nutrition_tracker/urls.py
+++ nutrition_tracker/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from api.views import index
 
 urlpatterns = [
   path('admin/', admin.site.urls),
   path('api/', include('api.urls')),
+  re_path('.*', index, name='index')
 ]
